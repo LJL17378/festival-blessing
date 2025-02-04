2415d4ad8608f269cf3ca6ddfbba16bdb6ada64d
masaobak <renderstraum@outlook.com>
2025-02-04 20:29:15 +0800

    修改响应格式

--- friends.go
+++ friends.go
@@ -25,19 +25,19 @@ func SendFriendRequest(c *gin.Context, db *gorm.DB) {
 		ToID int `json:"to_id"`
 	}
 	if err := c.ShouldBind(&request); err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid input"})
+		ResponseFAIL(c, http.StatusBadRequest, "Invalid input")
 		return
 	}
 	//是否已经发送请求
 	var existingRequest FriendRequest
 	if err := db.Where("from_id = ? and to_id = ?", fromID, request.ToID).First(&existingRequest).Error; err == nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": "你已经发送过请求了"})
+		ResponseFAIL(c, http.StatusBadRequest, "你已经发送过请求了")
 		return
 	}
 	//创建好友请求
 	friendRequest := FriendRequest{FromID: fromID, ToID: request.ToID}
 	db.Create(&friendRequest)
-	c.JSON(http.StatusOK, gin.H{"message": "已经发送添加请求"})
+	ResponseOK(c, nil, "已经发送添加请求")
 }
 
 func AcceptFriendRequest(c *gin.Context) {
@@ -46,13 +46,13 @@ func AcceptFriendRequest(c *gin.Context) {
 		RequestID int `json:"request_id"`
 	}
 	if err := c.ShouldBind(&request); err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid input"})
+		ResponseFAIL(c, http.StatusBadRequest, "Invalid input")
 		return
 	}
 	//查找好友请求
 	var friendRequest FriendRequest
 	if err := db.First(&friendRequest, request.RequestID).Error; err != nil || friendRequest.ToID != userID {
-		c.JSON(http.StatusNotFound, gin.H{"error": "没有找到请求"})
+		ResponseFAIL(c, http.StatusNotFound, "没有找到请求")
 		return
 	}
 	//接受请求
@@ -60,7 +60,7 @@ func AcceptFriendRequest(c *gin.Context) {
 	db.Create(&FriendRelationship{UserID: friendRequest.FromID, FriendID: friendRequest.ToID})
 	db.Create(&FriendRelationship{UserID: friendRequest.ToID, FriendID: friendRequest.FromID})
 
-	c.JSON(http.StatusOK, gin.H{"message": "已接受好友请求"})
+	ResponseOK(c, nil, "已接受好友请求")
 }
 
 func DeleteFriendRequest(c *gin.Context) {
@@ -69,12 +69,12 @@ func DeleteFriendRequest(c *gin.Context) {
 		FriendID int `json:"friend_id"`
 	}
 	if err := c.ShouldBind(&request); err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid input"})
+		ResponseFAIL(c, http.StatusBadRequest, "Invalid input")
 		return
 	}
 
 	db.Where("(user_id = ? and friend_id = ?) OR (friend_id = ? and user_id = ?)", userID, request.FriendID, userID, request.FriendID).Delete(&FriendRelationship{})
-	c.JSON(http.StatusOK, gin.H{"message": "好友已删除"})
+	ResponseOK(c, nil, "好友已经删除")
 }
 
 // 查询好友
@@ -83,27 +83,49 @@ func GetAllFriends(c *gin.Context) {
 	var friends []FriendRelationship
 	err := db.Where("user_id = ?", userID).Find(&friends).Error
 	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to retrieve friends"})
+		ResponseFAIL(c, http.StatusInternalServerError, "获取好友失败")
 		return
 	}
-	// 返回好友的用户id
-	var friendList []int
+
+	var friendList []struct {
+		ID       int    `json:"id"`
+		Username string `json:"username"`
+	}
+
 	for _, f := range friends {
+		var friend User
 		if f.UserID != userID {
-			friendList = append(friendList, f.UserID)
+			if err := db.First(&friend, f.UserID).Error; err == nil {
+				friendList = append(friendList, struct {
+					ID       int    `json:"id"`
+					Username string `json:"username"`
+				}{
+					ID:       friend.ID,
+					Username: friend.UserName,
+				})
+			}
 		} else {
-			friendList = append(friendList, f.FriendID)
+			if err := db.First(&friend, f.FriendID).Error; err == nil {
+				friendList = append(friendList, struct {
+					ID       int    `json:"id"`
+					Username string `json:"username"`
+				}{
+					ID:       friend.ID,
+					Username: friend.UserName,
+				})
+			}
 		}
 	}
-	c.JSON(http.StatusOK, gin.H{"friends": friendList})
+	ResponseOK(c, gin.H{"friends": friendList}, "获取好友列表成功")
 }
+
 func GetAllReceivedFriendRequests(c *gin.Context) {
 	userID := c.GetInt("userID")
 	// 查询当前用户收到的所有好友请求，且未接受的
 	var requests []FriendRequest
 	err := db.Where("to_id = ? AND accepted_status = ?", userID, false).Find(&requests).Error
 	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to retrieve friend requests"})
+		ResponseFAIL(c, http.StatusInternalServerError, "获取好友请求失败")
 		return
 	}
 	// 组合好友请求信息
@@ -119,5 +141,5 @@ func GetAllReceivedFriendRequests(c *gin.Context) {
 			"username":   user.UserName,
 		})
 	}
-	c.JSON(http.StatusOK, gin.H{"requests": requestList})
+	ResponseOK(c, gin.H{"requests": requestList}, "成功获取所有未接受的好友请求")
 }
