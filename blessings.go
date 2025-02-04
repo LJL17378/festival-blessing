14622a99cbbc2887ccd6b6b335398969cbc902b2
masaobak <renderstraum@outlook.com>
2025-02-04 19:53:00 +0800

    添加祝福字段，修改响应格式

--- blessings.go
+++ blessings.go
@@ -11,6 +11,8 @@ type Blessing struct {
 	SenderID   int       `gorm:"not null"`
 	ReceiverID int       `gorm:"not null"`
 	Content    string    `gorm:"not null"`
+	Font       string    `gorm:"not null"`
+	PaperStyle string    `gorm:"not null"`
 	CreatedAt  time.Time `gorm:"autoCreateTime"`
 }
 
@@ -18,10 +20,12 @@ func SendBlessings(c *gin.Context) {
 	var req struct {
 		ReceiverID int    `json:"receiver_id"`
 		Content    string `json:"content"`
+		Font       string `json:"font"`
+		PaperStyle string `json:"paper_style"`
 	}
 
 	if err := c.ShouldBindJSON(&req); err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid request format"})
+		ResponseFAIL(c, http.StatusBadRequest, "Invalid request format")
 		return
 	}
 
@@ -31,7 +35,7 @@ func SendBlessings(c *gin.Context) {
 	var count int64
 	db.Table("friend_relationships").Where("(user_id = ? and friend_id = ?) or (user_id = ? and friend_id = ?)", senderID, req.ReceiverID, req.ReceiverID, senderID).Count(&count)
 	if count == 0 {
-		c.JSON(http.StatusForbidden, gin.H{"error": "对方不是你的好友"})
+		ResponseFAIL(c, http.StatusForbidden, "对方不是你的好友")
 		return
 	}
 
@@ -39,14 +43,16 @@ func SendBlessings(c *gin.Context) {
 		SenderID:   senderID,
 		ReceiverID: req.ReceiverID,
 		Content:    req.Content,
+		Font:       req.Font,
+		PaperStyle: req.PaperStyle,
 	}
 
 	if err := db.Create(&blessing).Error; err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"error": "发送祝福失败"})
+		ResponseFAIL(c, http.StatusInternalServerError, "发送祝福失败")
 		return
 	}
 
-	c.JSON(http.StatusOK, gin.H{"message": "祝福发送成功"})
+	ResponseOK(c, gin.H{"blessings_sent": blessing}, "祝福发送成功")
 }
 
 // 查询自己发送的祝福
@@ -57,21 +63,25 @@ func GetSentBlessings(c *gin.Context) {
 		ReceiverID   int    `json:"receiver_id"`
 		ReceiverName string `json:"receiver_name"`
 		Content      string `json:"content"`
+		Font         string `json:"font"`
+		PaperStyle   string `json:"paper_style"`
 		Timestamp    string `json:"timestamp"`
 	}
 	err := db.Table("blessings").
-		Select("blessings.receiver_id, users.user_name as receiver_name, blessings.content, blessings.created_at as timestamp").
+		Select("blessings.receiver_id, users.user_name as receiver_name, blessings.content, blessings.font, blessings.paper_style, blessings.created_at as timestamp").
 		Joins("JOIN users ON blessings.receiver_id = users.id").
 		Where("blessings.sender_id = ?", userID).
 		Order("blessings.created_at DESC").
 		Scan(&blessings).Error
 
 	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to retrieve sent blessings"})
+		ResponseFAIL(c, http.StatusInternalServerError, "无法获取发出的祝福")
 		return
 	}
 
-	c.JSON(http.StatusOK, gin.H{"sent_blessings": blessings})
+	ResponseOK(c, gin.H{
+		"sent_blessings": blessings,
+	}, "查询祝福成功")
 }
 
 // 查询自己收到的祝福
@@ -82,20 +92,24 @@ func GetReceivedBlessings(c *gin.Context) {
 		SenderID   uint   `json:"sender_id"`
 		SenderName string `json:"sender_name"`
 		Content    string `json:"content"`
+		Font       string `json:"font"`
+		PaperStyle string `json:"paper_style"`
 		Timestamp  string `json:"timestamp"`
 	}
 
 	err := db.Table("blessings").
-		Select("blessings.sender_id, users.user_name as sender_name, blessings.content, blessings.created_at as timestamp").
+		Select("blessings.sender_id, users.user_name as sender_name, blessings.content, blessings.font, blessings.paper_style, blessings.created_at as timestamp").
 		Joins("JOIN users ON blessings.sender_id = users.id").
 		Where("blessings.receiver_id = ?", userID).
 		Order("blessings.created_at DESC").
 		Scan(&blessings).Error
 
 	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to retrieve received blessings"})
+		ResponseFAIL(c, http.StatusInternalServerError, "无法获取收到的祝福")
 		return
 	}
 
-	c.JSON(http.StatusOK, gin.H{"received_blessings": blessings})
+	ResponseOK(c, gin.H{
+		"received_blessings": blessings,
+	}, "查询成功")
 }
